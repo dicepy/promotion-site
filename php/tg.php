05613ce6d5d732dc04ba6d875e03c80b1efae09a
dicepy <mdi.2005@mail.ru>
2023-07-08 23:17:58 +0300

    animation

---
+++ php/tg.php
@@ -0,0 +1,27 @@
+<?php 
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$comment = $_POST['user_comment'];
+$pagefrom = $_POST['pagefrom'];
+$token = "6069651084:AAGDqmnDXgu59pZ7k1do4hifjNPKKu6EfR4";
+$chat_id = "-662471046";
+$arr = array(
+    'Имя пользователя: ' => $name,
+    'Телефон: ' => '+' .$phone,
+    'Комментарий:' => $comment,
+    'Страница заявки: ' => $pagefrom
+  );
+  
+  foreach($arr as $key => $value) {
+    $txt .= "<b>".$key."</b> ".$value."%0A";
+  };
+  
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+    header('Location: index.html');
+  } else {
+    echo "Error";
+  }
+?>
--- script/script.js
+++ script/script.js
@@ -1,15 +1,25 @@
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
+  
+
   services = document.querySelector('.services');
   for (el of document.querySelectorAll('button.button')){
     id = el.id;
     $(`#${id}`).on( "click", function(event) {
       console.log(event.currentTarget.id);
       event.preventDefault();
-      $(`.modalwindoworder`).css('display', 'flex')
+      $(`.modalwindoworder`).css('opacity', '0');
+      $(`.modalwindoworder`).css('display', 'flex');
+      $(`.modalwindoworder`).animate({opacity:1},300);
     } );
     $(`.modalwindoworder`).on( "click", function(event) {
       if ($(event.target).attr('class')=='modalwindoworder'){
-        $(`.modalwindoworder`).css('display', 'none')
+        $(`.modalwindoworder`).css('opacity', '1');
+        $(`.modalwindoworder`).animate({opacity:0},300);
+        setTimeout(function(){
+          $(`.modalwindoworder`).css('display', 'none');
+        },300)
       }
     })
   }
