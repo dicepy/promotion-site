0ba480c349c38be41d4a404b966253fbd632ecc1
dicepy <mdi.2005@mail.ru>
2023-07-09 19:27:43 +0300

    modal + form started

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  
+  window_title_text = document.querySelector('.window__title').textContent;
+  console.log(window_title_text)
 
   services = document.querySelector('.services');
   for (el of document.querySelectorAll('button.button')){
@@ -14,11 +15,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
       $(`.modalwindoworder`).animate({opacity:1},300);
     } );
     $(`.modalwindoworder`).on( "click", function(event) {
-      if ($(event.target).attr('class')=='modalwindoworder'){
+      if ($(event.target).attr('class')=='modalwindoworder' | $(event.target).attr('class')=='window__close'){
         $(`.modalwindoworder`).css('opacity', '1');
         $(`.modalwindoworder`).animate({opacity:0},300);
         setTimeout(function(){
-          $(`.modalwindoworder`).css('display', 'none');
+        $(`.modalwindoworder`).css('display', 'none');
         },300)
       }
     })
